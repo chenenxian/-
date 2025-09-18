225311f53f0ce3f67b5f8ba5357781ec2a5745f0
chenenxian <18175392699@163.com>
2025-09-18 21:09:54 +0800

    Add files via upload
    练习一

---
+++ 2025.9.18练习一.py
@@ -0,0 +1,13 @@
+name=input("请输入员工的姓名:")
+age=input("请输入员工的年龄:")
+sex=input("请输入员工的性别:")
+Id=input("请输入员工的工号:")
+print(name,age,sex,Id)
+
+
+a=int(3)
+b=int(2)
+c=float(2.7)
+print(a+b+c)
+d=a+b+c
+print(type(d))
